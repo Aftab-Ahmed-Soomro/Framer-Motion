bde6949bb46feb79ebfef3a4b6d64b82ccc884de
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-09-25 15:57:57 -0700

    fm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { motion } from "motion/react"
+import { motion, useScroll } from "motion/react"
 
 const App = () => {
-  return (
-    <div>
+  // const scrollYProgress = useScroll().scrollYProgress
+  const {scrollYProgress} = useScroll()
 
-      <motion.div 
+  console.log(scrollYProgress)
+
+  return (
+    <div className='p-20 text-center text-white font-mono'>
+
+      {/* <motion.div 
       className="box"
       drag
       whileDrag={{
@@ -23,10 +28,9 @@ const App = () => {
         top: 0,
         bottom: 300
       }}
-      // dragDirectionLock= "true"
+      dragDirectionLock= "true"
       >
-      
-      </motion.div>
+      </motion.div> */}
 
       {/* <motion.div 
       className='box'
@@ -106,6 +110,18 @@ const App = () => {
         y: 200,
       }}>
       </motion.div> */}
+
+      {/* ************************ Scroll Effect Animation *********************** */}
+      <motion.div 
+      style={{scaleX: scrollYProgress}}
+      className='bg-red-500 origin-left w-full h-3 fixed top-0 left-0'
+      >
+
+      </motion.div>
+      <h2 className='font-bold text-4xl mb-8'>Aftab Ahmed Soomro</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse praesentium corrupti impedit atque eos adipisci inventore, nostrum quidem iure repudiandae corporis nemo quasi reiciendis veritatis, numquam explicabo expedita similique sit ad, dolores non. Et officiis asperiores, accusamus soluta pariatur velit numquam saepe debitis assumenda animi totam consectetur dolorem temporibus qui dolore non explicabo cumque veritatis eveniet commodi minus quas doloribus. Ipsam maiores autem, illo voluptate quam corporis, ipsum enim explicabo dolorem possimus quae labore optio minus, ullam nam corrupti facere perferendis cupiditate sequi. Temporibus at aliquid incidunt eveniet rerum provident maxime beatae molestias omnis rem magni odit hic, et vero? <br /><br /><br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, vel deserunt facilis totam similique harum mollitia hic eos atque. Ab, saepe quia! Qui, repudiandae unde veritatis eius id rerum repellat distinctio itaque sapiente laborum perferendis soluta, quia cupiditate doloremque est eum nisi earum neque placeat molestiae dolor? Cum reiciendis et ipsa mollitia at error accusantium doloremque, molestiae asperiores quo ipsum iusto quos quis non rerum cumque repellat exercitationem perspiciatis culpa similique qui ea impedit? Vero ipsum fugiat nam quasi saepe repudiandae enim minus expedita voluptatibus dolorem commodi facilis officia, et nisi cumque harum delectus corrupti, est quas. Doloremque cum enim modi quia consequatur cumque architecto perspiciatis numquam esse. In quae aliquid repudiandae delectus ipsa mollitia quasi. Sed, doloribus saepe. Adipisci, nobis nostrum tempora sit dolor molestias error dolore quod iusto qui, dicta illo, ratione at dolorum provident! Sed quibusdam totam veniam nesciunt? Debitis doloribus doloremque modi. Explicabo corporis modi quisquam est, dolor blanditiis distinctio sint, aperiam recusandae animi repudiandae! Magni vitae dolores cumque deleniti non! Reprehenderit omnis placeat labore dolorem, culpa similique molestias voluptatum porro, sunt fugit in quisquam, autem ducimus illum adipisci. Tempore doloribus recusandae explicabo incidunt quas voluptatem optio veniam veritatis, animi, maiores aspernatur minus. Laboriosam, expedita similique. <br /><br /><br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ut dolorem magni et repudiandae officia, placeat adipisci deleniti ad aliquid pariatur harum ex amet explicabo minima, quos, delectus voluptate nisi exercitationem beatae! Magnam minima quisquam dignissimos unde laborum accusantium velit voluptate alias in incidunt. Minus minima, ratione vel libero quia dolorum explicabo quibusdam. Aperiam esse ducimus quos expedita quaerat obcaecati asperiores non commodi? Eius, itaque qui? Porro ex doloremque iusto minus odit atque consequatur, quibusdam laboriosam eveniet? Illo optio error esse ipsam voluptates, dolorem culpa libero expedita doloribus laboriosam earum amet quod iste laborum. Facilis totam aspernatur ut voluptas at quam, sunt nihil iure saepe placeat, voluptates atque! Doloremque et reprehenderit maiores quis doloribus sapiente, provident corporis, sed placeat ab beatae ex assumenda ea laborum. Quia accusantium assumenda repudiandae eius aliquam, aspernatur vero dolore nobis? Ab quo quis aliquid reiciendis nemo qui aspernatur, perferendis voluptatem sed minus porro sapiente placeat beatae fugiat at ducimus dolores hic culpa sit vitae voluptatum eaque deserunt quia labore? Corrupti molestias vel, impedit voluptate voluptas vitae ratione assumenda neque dolorum accusantium! Assumenda cumque laboriosam dolorem ullam facilis illo velit commodi illum, quis natus dolor quia. Nostrum fugiat corrupti voluptates, explicabo nemo quasi similique velit unde quos natus, eveniet aut eos ab porro nihil incidunt facere libero fugit eaque quaerat esse? Ut facere recusandae ducimus ad culpa iste temporibus laudantium, architecto earum quasi molestias accusantium, debitis libero fuga quod, suscipit excepturi delectus itaque. Quibusdam deleniti ullam, adipisci quod.</p>
     </div>
   )
 }
